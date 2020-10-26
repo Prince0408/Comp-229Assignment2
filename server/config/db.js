02f20fb94019406f3ffd4041e08e64afd22790e2
Prince0408 <69872209+Prince0408@users.noreply.github.com>
2020-10-25 20:49:49 -0400

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports =
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://gurkiratkhaira:V2BSPPUTe0FpxOfG@databaseforherokuassign.hpq66.mongodb.net/contact_list?retryWrites=true&w=majority"
+}
+
+
